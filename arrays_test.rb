58afa019423377268c23868795ced85c451f584e
Joshua Madden <jmaddenco@gmail.com>
2015-09-18 10:15:57 -0600

    tests for max created

--- arrays_test.rb
+++ arrays_test.rb
@@ -16,4 +16,17 @@ class ArraysTest < MiniTest::Test
 		assert_equal [5, 5], @m.middles([1, 5, 0], [2, 5, 8])
 	end
 
+	def test_max
+		assert_equal 5, @m.middles([1, 2, 3, 4, 5])
+		assert_equal 12, @m.middles([1, 1, 3, 5, 12])
+		assert_equal 9, @m.middles([1, 3, 9, 1, 4, 5])
+		assert_equal 4, @m.middles([1, 2, 4, 1, 1, 0, 1])
+		
+	end
+
+	# def test_luck13
+	# 	assert_equal [], @m.middles([])
+
+	# end
+
 end
